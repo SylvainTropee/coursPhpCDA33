c50bbbf8a39b18520275cb9c71fa22b15e9060d9
stropee <stropee@campus-eni.fr>
2024-10-14 13:58:03 +0200

    Les bases (variables, tableaux...)

---
+++ module_2/demo3/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'other.php';
+
+//tableau
+$aName = [];
+$aName[] = "Michel";
+$aName[] = "Robert";
+$aName[] = "Chantal";
+
+$aCity = ["name" => "Rennes", "population" => 250000];
+
+var_dump($aCity);
+var_dump($other);
